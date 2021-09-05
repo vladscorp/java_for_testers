0418ee336ee8050d2e73ee257dacf942bfe24f98
vladscorp <v1adscorp@mail.ru>
2021-09-05 14:59:24 +0300

    Revert "рефакторинг EntryCreationTests"
    This reverts commit abc58147ca88026c80ff2ea07f8852b195f1e062.

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/EntryCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/EntryCreationTests.java
@@ -1,10 +1,9 @@
 package ru.stqa.jft.addressbook;
 
 import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class EntryCreationTests {
@@ -12,76 +11,58 @@ public class EntryCreationTests {
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    wd = new ChromeDriver();
+    wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testEntryCreation() throws Exception {
-    gotoNewEntryPage();
-    fillEntryForm(new EntryData("Ivan", "Aleksandrovich", "Petrov", "vanko", "title", "comp", "блаблабла очень длинный адрес 23", "123345", "123156496879", "wqer@qwe.ru", "16", "September", "1980"));
-    submitEntryCreation();
-    returnToHomePage();
-  }
-
-  private void returnToHomePage() {
-    wd.findElement(By.linkText("home page")).click();
-  }
-
-  private void submitEntryCreation() {
-    wd.findElement(By.xpath("//input[21]")).click();
-  }
-
-  private void fillEntryForm(EntryData entryData) {
+    wd.get("http://localhost/addressbook/");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.id("LoginForm")).submit();
+    wd.findElement(By.linkText("add new")).click();
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(entryData.firstname());
+    wd.findElement(By.name("firstname")).sendKeys("Ivan");
     wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(entryData.middlename());
+    wd.findElement(By.name("middlename")).sendKeys("Aleksandrovich");
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(entryData.lastname());
+    wd.findElement(By.name("lastname")).sendKeys("Petrov");
     wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(entryData.nickname());
+    wd.findElement(By.name("nickname")).sendKeys("vanko");
     wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(entryData.title());
+    wd.findElement(By.name("title")).sendKeys("title");
     wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(entryData.company());
+    wd.findElement(By.name("company")).sendKeys("comp");
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(entryData.address());
+    wd.findElement(By.name("address")).sendKeys("блаблабла очень длинный адрес 23");
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(entryData.home());
+    wd.findElement(By.name("home")).sendKeys("123345");
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(entryData.mobile());
+    wd.findElement(By.name("mobile")).sendKeys("123156496879");
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(entryData.email());
+    wd.findElement(By.name("email")).sendKeys("wqer@qwe.ru");
     wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(entryData.bday());
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText("16");
     wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(entryData.bmonth());
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("September");
     wd.findElement(By.name("byear")).click();
     wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(entryData.byear());
-  }
-
-  private void gotoNewEntryPage() {
-    wd.findElement(By.linkText("add new")).click();
+    wd.findElement(By.name("byear")).sendKeys("1980");
+    wd.findElement(By.name("theform")).click();
+    wd.findElement(By.xpath("//input[21]")).click();
+    wd.findElement(By.linkText("home page")).click();
+    wd.findElement(By.linkText("Logout")).click();
+    wd.findElement(By.xpath("//*/text()[normalize-space(.)='']/parent::*")).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
-    wd.findElement(By.linkText("Logout")).click();
     wd.quit();
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/EntryData.java
+++
@@ -1,6 +0,0 @@
-package ru.stqa.jft.addressbook;
-
-public record EntryData(String firstname, String middlename, String lastname, String nickname, String title,
-                        String company, String address, String home, String mobile, String email, String bday,
-                        String bmonth, String byear) {
-}
