9ea417b3094f1f20e90cdc6a9522301f102b6aee
vladscorp <v1adscorp@mail.ru>
2021-11-13 15:08:10 +0300

    Реализована возможность запуска в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/TestBase.java
@@ -1,6 +1,7 @@
 package ru.stqa.jft.addressbook.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -9,7 +10,7 @@ import ru.stqa.jft.addressbook.model.EntryData;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
