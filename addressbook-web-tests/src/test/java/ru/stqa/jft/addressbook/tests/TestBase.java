761d3e2d723890dba7ce81a349232441c6d8922a
vladscorp <v1adscorp@mail.ru>
2021-12-01 18:32:47 +0300

    настройка логов и hibernate

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/TestBase.java
@@ -2,11 +2,20 @@ package ru.stqa.jft.addressbook.tests;
 
 import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.jft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -18,6 +27,18 @@ public class TestBase {
     @AfterSuite(alwaysRun = true)
       public void tearDown() throws Exception {
         app.stop();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " +m.getName() + " with parameters " + Arrays.asList(p));
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " +m.getName());
+
     }
 
 
